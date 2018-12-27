e85d7a2b6644de8d1a991532325ed5fd2eb70c8b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-12-27 19:53:24 +0100

    Replace Phake with Phrophecy

---
+++ tests/ClientStub.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace WyriHaximus\Tests\React\Cache;
+
+use Clue\React\Redis\Client;
+use Evenement\EventEmitter;
+use React\Promise\PromiseInterface;
+use function React\Promise\resolve;
+
+class ClientStub extends EventEmitter implements Client
+{
+    public function __call($name, $args)
+    {
+        // TODO: Implement __call() method.
+    }
+
+    public function end()
+    {
+        // TODO: Implement end() method.
+    }
+
+    public function close()
+    {
+        // TODO: Implement close() method.
+    }
+
+    public function exists(): PromiseInterface
+    {
+        return resolve();
+    }
+
+    public function get(): PromiseInterface
+    {
+        return resolve();
+    }
+
+    public function set(): PromiseInterface
+    {
+        return resolve();
+    }
+
+    public function expire(): PromiseInterface
+    {
+        return resolve();
+    }
+
+    public function del(): PromiseInterface
+    {
+        return resolve();
+    }
+}
